0a60bd63cbcd57ceda20ac33b2741b3f3038e48a
Shubhra7 <shubhrajit.g2002@gmail.com>
2025-04-23 10:11:40 +0530

    user controller  bug fixed

--- Seven_video/src/controllers/user.controller.js
+++ Seven_video/src/controllers/user.controller.js
@@ -291,7 +291,11 @@ const changeCurrentPassword = asyncHandler(async (req,res)=>{
 const getCurrentUser = asyncHandler(async (req,res)=>{
     return res
     .status(200) // as auth.middleware will inject req.user
-    .json(200,req.user, "Current user fetched successfully.")
+    .json( new ApiResponse(
+        200,
+        req.user,
+        "Current user fetched successfully."
+    ))
 })
 
 const updateAccountDetails = asyncHandler(async (req,res)=>{
@@ -301,7 +305,7 @@ const updateAccountDetails = asyncHandler(async (req,res)=>{
         throw new ApiError(400, "All fields are required!")
     }    
 
-    const user = User.findByIdAndUpdate(
+    const user = await User.findByIdAndUpdate(
         req.user?._id,
         {
             $set: {
